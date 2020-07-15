34274c0cec982d04522beffbd6f557811d8f73a9
tuti <ptutino@gmail.com>
2020-07-15 12:36:01 -0300

    Video embed

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -21,8 +21,23 @@ const Header = () => {
     }
   }, []);
 
+  // <video loop muted autoPlay poster="img/videoframe.jpg" className="fullscreen-bg__video">
+  // <source src="video/bg.mp4" type="video/mp4" />
+  // <source src="https://assets.mixkit.co/videos/preview/mixkit-sun-rays-over-forest-treetops-515-large.mp4" type="video/mp4" />
+  // hd https://lh3.googleusercontent.com/FjqufdNxsxsV_xUNmGabh_vsOx4r_VRfBU11lfs2IYLzfRMtR922eAHPn7ZaWzggbm8cxwMpp5cGJj6vG5YKYitSf2P3tjmCPp_DevbpoC0QCeDCb7xXdhhUY1rPEW8a_7X9LS9BTQI=m37
   return (
     <section id="hero" className="jumbotron">
+      <div className="fullscreen-bg">
+        <video loop muted autoPlay className="fullscreen-bg__video">
+          <source src="https://drive.google.com/uc?export=download&id=1Yc8zVbt3pF8lr5AtKXIhlKtQdpnv8Vl-" type="video/mp4" />
+        
+        
+
+        </video>
+      </div>
+      <div className="fullscreen-bg-logo">
+        <div className="fullscreen-bg__logo" id="logo" />
+      </div>
       <Container>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
